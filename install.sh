46c1ff85ba68c647dae9cce584987fe6fcef213f
r0dok <93918376+r0dok@users.noreply.github.com>
2022-04-28 16:22:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+USER="$(whoami)"
+HOMEDIR="/home/${USER}"
+
+
 AUDIO_SRC='https://www.youtube.com/watch?v=5Z5qYOuc1dE'
-AUDIO_DST='~/ara_ara.mp3'
+AUDIO_DST="${HOMEDIR}/Music/ara_ara.mp3"
 
 # Ara_ara.mp3:
 yes | pacman -S youtube-dl mpv
@@ -14,17 +18,17 @@ yes | pacman -Syyu
 yes | pacman -S discord neofetch steam bashtop asciiquarium
 
 # bonsai:
-mkdir ~/{Notes,Tools,Visuals}
-git clone https://gitlab.com/jallbrit/cbonsai ~/Tools/
-cd ~/Tools/cbonsai/
-make install PREFIX=~/.local
+mkdir ${HOMEDIR}/{Notes,Tools,Visuals}
+git clone https://gitlab.com/jallbrit/cbonsai ${HOMEDIR}/Tools/
+cd ${HOMEDIR}/Tools/cbonsai/
+make install PREFIX=${HOMEDIR}/.local
 make install
 
 # GUI themes:
-git clone https://github.com/addy-dclxvi/openbox-theme-collections ~/.themes
+git clone https://github.com/addy-dclxvi/openbox-theme-collections ${HOMEDIR}/.themes
 
 # Blackarch:
-cd Tools/
+cd ${HOMEDIR}/Tools/
 curl -O https://blackarch.org/strap.sh
 chmod +x strap.sh
 yes | pacman -S catphish email2phonenumber holehe seeker simpleemailspoofer buster whatsmyname whatbreach twint
@@ -36,7 +40,7 @@ amixer set Master 50%
 
 # Greetings.mp3:
 AUDIO_SRC='https://www.youtube.com/watch?v=kOKneP3tH90'
-AUDIO_DST='~/greetings_master.mp3'
+AUDIO_DST='${HOMEDIR}/Music/greetings_master.mp3'
 youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output
 "${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
 mpv "${AUDIO_DST}"
