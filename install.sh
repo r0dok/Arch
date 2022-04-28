c0f3c94951323adc3b2e4c5470ebd1f694c7d688
r0dok <93918376+r0dok@users.noreply.github.com>
2022-04-28 16:14:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-AUDIO_SRC='https://www.youtube.com/watch?v=kOKneP3tH90'
-AUDIO_DST='~/greetings_master.mp3'
-mkdir ~/{Notes,Tools,Visuals}
+AUDIO_SRC='https://www.youtube.com/watch?v=5Z5qYOuc1dE'
+AUDIO_DST='~/ara_ara.mp3'
 
+# Ara_ara.mp3:
+yes | pacman -S youtube-dl mpv
+youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output
+"${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
+mpv "${AUDIO_DST}"
+
+# Regular tools:
 yes | pacman -Syyu
-yes | pacman -S discord neofetch steam bashtop asciiquarium youtube-dl mpv
+yes | pacman -S discord neofetch steam bashtop asciiquarium
 
+# bonsai:
+mkdir ~/{Notes,Tools,Visuals}
 git clone https://gitlab.com/jallbrit/cbonsai ~/Tools/
 cd ~/Tools/cbonsai/
 make install PREFIX=~/.local
 make install
 
+# GUI themes:
 git clone https://github.com/addy-dclxvi/openbox-theme-collections ~/.themes
 
+# Blackarch:
 cd Tools/
 curl -O https://blackarch.org/strap.sh
 chmod +x strap.sh
-
 yes | pacman -S catphish email2phonenumber holehe seeker simpleemailspoofer buster whatsmyname whatbreach twint
 trape social-vuln-scanner
 
@@ -25,7 +34,9 @@ trape social-vuln-scanner
 amixer set Master on
 amixer set Master 50%
 
-# Get and play the audio file:
+# Greetings.mp3:
+AUDIO_SRC='https://www.youtube.com/watch?v=kOKneP3tH90'
+AUDIO_DST='~/greetings_master.mp3'
 youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output
 "${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
 mpv "${AUDIO_DST}"
