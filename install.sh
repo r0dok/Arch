e61518cad0670306aaeaa930152f745588ee752d
r0dok <93918376+r0dok@users.noreply.github.com>
2022-05-05 11:59:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,7 @@ yes | pacman -Syyu
 
 # Ara_ara.mp3:
 yes | pacman -S --needed ffmpeg youtube-dl mpv
-youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output
-"${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
+youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output "${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
 mpv "${AUDIO_DST}"
 
 # Regular tools:
@@ -41,8 +40,7 @@ amixer set Master 50%
 # Greetings.mp3:
 AUDIO_SRC='https://www.youtube.com/watch?v=kOKneP3tH90'
 AUDIO_DST='${HOMEDIR}/Music/greetings_master.mp3'
-youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output
-"${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
+youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output "${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
 mpv "${AUDIO_DST}"
 
 echo "You are done now, enjoy!"
