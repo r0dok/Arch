7b583044fa513114494ad204e9804e1766c7a414
r0dok <93918376+r0dok@users.noreply.github.com>
2024-06-20 12:56:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,51 @@
 #!/bin/bash
 
-USER="$(readlink -f $(whoami) | cut -d '/' -f 3)"
+USER=$(whoami)
 HOMEDIR="/home/${USER}"
 
+# Update system and install required packages
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm ffmpeg youtube-dl mpv discord neofetch steam bashtop asciiquarium git curl
+
+# Function to download and play audio
+download_and_play_audio() {
+  local AUDIO_SRC=$1
+  local AUDIO_DST=$2
+  
+  youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output "${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
+  mpv "${AUDIO_DST}"
+}
+
+# Download and play "ara_ara.mp3"
 AUDIO_SRC='https://www.youtube.com/watch?v=5Z5qYOuc1dE'
 AUDIO_DST="${HOMEDIR}/Music/ara_ara.mp3"
+download_and_play_audio "${AUDIO_SRC}" "${AUDIO_DST}"
 
+# Download and play "greetings_master.mp3"
+AUDIO_SRC='https://www.youtube.com/watch?v=kOKneP3tH90'
+AUDIO_DST="${HOMEDIR}/Music/greetings_master.mp3"
+download_and_play_audio "${AUDIO_SRC}" "${AUDIO_DST}"
 
-# Ara_ara.mp3:
-youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output "${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
-mpv "${AUDIO_DST}"
+# Create directories
+mkdir -p ${HOMEDIR}/{Notes,Tools,Visuals}
 
-# bonsai:
-mkdir ${HOMEDIR}/{Notes,Tools,Visuals}
-git clone https://gitlab.com/jallbrit/cbonsai ${HOMEDIR}/Tools/
-cd ${HOMEDIR}/Tools/cbonsai/
+# Clone and install cbonsai
+git clone https://gitlab.com/jallbrit/cbonsai ${HOMEDIR}/Tools/cbonsai
+cd ${HOMEDIR}/Tools/cbonsai
 make install PREFIX=${HOMEDIR}/.local
 make install
 
-# GUI themes:
+# Clone GUI themes
 git clone https://github.com/addy-dclxvi/openbox-theme-collections ${HOMEDIR}/.themes
 
-# Setting up audio:
+# Setup audio levels
 amixer set Master on
 amixer set Master 50%
 
-# Greetings.mp3:
-AUDIO_SRC='https://www.youtube.com/watch?v=kOKneP3tH90'
-AUDIO_DST='${HOMEDIR}/Music/greetings_master.mp3'
-youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output "${AUDIO_DST}" --no-playlist "${AUDIO_SRC}"
-mpv "${AUDIO_DST}"
+# Install BlackArch tools
+curl -O https://blackarch.org/strap.sh
+chmod +x strap.sh
+./strap.sh
+yes | sudo pacman -S --needed catphish email2phonenumber holehe seeker simpleemailspoofer buster whatsmyname whatbreach twint trape social-vuln-scanner
 
-echo "You are done now, enjoy!"
+echo "Setup is complete. Enjoy your system!"
